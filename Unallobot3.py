54fcae5611d19f4e41ba8730448a95a70b29587c
Dalton Schaadt <dschaadt@Daltons-MacBook-Pro.local>
2014-11-08 17:13:00 -0500

    Being anal-retentive.

--- Unallobot3.py
+++ Unallobot3.py
@@ -4,26 +4,33 @@
 
 import socket
 
+
 def ping(): # Responding to Server Pings
 	ircsock.send("PONG :Pong\n")
+
 
 def sendmsg (chan):
 	ircsock.send("PRIVMSG "+ chan +" :" + msg + "\n")
 
-def Eightball (data):
+
+def eightball (data):
 	if data!='' and '?' in data:
 			return choice(['It is certain.','It is decidedly so.','Without a doubt.','Yes. definitely.','You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Signs point to yes.','Yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.','Don\'t count on it.','My reply is no.','My sources say no.','Outlook not so good.','Very doubtful.','Run Away!']) 
 	else:
 		return 'I can do nothing unless you ask me a question....'
 
-def Address ():
+
+def address ():
 	return "512 Shaw Court #105, Severn, MD 21144"
 
-def Sign (data): # Check the sign message or Change the sign Message
 
-def Status (): # Check the Status of the space
+def sign (data): # Check the sign message or Change the sign Message
 
-def Help (data):
+
+def status (): # Check the Status of the space
+
+
+def help (data):
 			data=data.replace('.','')
 		helps={
 				'status':'!status returns the current status of the space. (open/closed)',
